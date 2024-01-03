93c7d339782fffef866ee9dd70a0370963254414
Ngoc Quang <nguyenngocquang08092002@gmail.com>
2024-01-03 09:29:06 +0700

    update handle excel file

--- front-end/vue-project/src/components/FileUpload.vue
+++ front-end/vue-project/src/components/FileUpload.vue
@@ -1,116 +1,120 @@
 <template>
     <div class="file-upload">
-        <div class="file-upload__area">
-            <div>
-                <label for="file" class="file-upload-label">Select file</label>
-                <input type="file" name="" id="file" accept=".xlsx" />
-            </div>
-            <button class="button-upload" @click="uploadFile">Upload file</button>
-            <div class="flex-container">
-                <h5 v-if="showDownloadButton === true">Invalid file, please download and check it</h5>
-                <button v-if="showDownloadButton === true" @click="downloadErrorFile" class="button-download">Download</button>
-            </div>
-
+      <div class="file-upload__area">
+        <div>
+          <label for="file" class="file-upload-label">Select file</label>
+          <input type="file" name="" id="file" accept=".xlsx" />
         </div>
+        <button class="button-upload" @click="uploadFile">Upload file</button>
+        <div class="flex-container">
+          <h4 v-if="showDownloadButton === true">Invalid file, please download and check it</h4>
+          <button v-if="showDownloadButton === true" @click="downloadErrorFile" class="button-download">Download</button>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
   
-<script>
-export default {
+  <script>
+  import * as XLSX from 'xlsx';
+  
+  export default {
     name: "FileUpload",
     data() {
-        return {
-            lengthError: 0,
-            showDownloadButton: false,
-        };
+      return {
+        lengthError: 0,
+        showDownloadButton: false,
+      };
     },
     methods: {
-        uploadFile() {
-            const response = {
-                "lengthError": 5,
-                "name": "File name <= 250 , file extension phải là xlsx ",
-                "precondition": "chỉ có 2 enum là enable và disable",
-            };
-
-            this.lengthError = response.lengthError;
-
-            this.checkError();
-        },
-        downloadErrorFile() {
-            const errorData = this.generateErrorFileContent();
-            const ws = XLSX.json_to_sheet([errorData]);
-
-            const wb = XLSX.utils.book_new();
-            XLSX.utils.book_append_sheet(wb, ws, 'Error Report');
-
-            const blob = XLSX.write(wb, {bookType: 'xlsx', type: 'blob'});
-
-            const a = document.createElement('a');
-            const blobURL = URL.createObjectURL(blob);
-            a.href = blobURL;
-            a.download = 'errorFile.xlsx';
-
-            document.body.appendChild(a);
-            a.click();
-
-            document.body.removeChild(a);
-            URL.revokeObjectURL(blobURL);
-        },
-        checkError() {
-            if (this.lengthError > 0) {
-                // Hiển thị nút download
-                this.showDownloadButton = true;
-            } else {
-                // Ẩn nút download
-                this.showDownloadButton = false;
-            }
-        },
-        generateErrorFileContent() {
-            const errorData = {
-                "lengthError": 5,
-                "name": "File name <= 250 , file extension phải là xlsx ",
-                "precondition": "chỉ có 2 enum là enable và disable",
-            };
-
-            return {
-                "Total Errors: ": errorData.lengthError,
-                ...errorData,
-            };
-        },
-    },
-};
-</script>
+      uploadFile() {
+        const response = {
+          "lengthError": 5,
+          "name": "File name <= 250 , file extension phải là xlsx ",
+          "precondition": "chỉ có 2 enum là enable và disable",
+        };
   
-<style scoped>
-.file-upload {
+        this.lengthError = response.lengthError;
+  
+        this.checkError();
+      },
+      downloadErrorFile() {
+        const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+
+        const errorData = this.generateErrorFileContent();
+        const worksheet = XLSX.utils.json_to_sheet([errorData]);
+  
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Error Report');
+  
+        const blobBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        const blob = new Blob([blobBuffer], {type: fileType})
+
+        const a = document.createElement('a');
+        const blobURL = URL.createObjectURL(blob);
+        a.href = blobURL;
+        a.download = 'errorFile.xlsx';
+  
+        document.body.appendChild(a);
+        a.click();
+  
+        document.body.removeChild(a);
+        URL.revokeObjectURL(blobURL);
+      },
+      checkError() {
+        if (this.lengthError > 0) {
+          // Hiển thị nút download
+          this.showDownloadButton = true;
+        } else {
+          // Ẩn nút download
+          this.showDownloadButton = false;
+        }
+      },
+      generateErrorFileContent() {
+        const errorData = {
+          "lengthError": 5,
+          "name": "File name <= 250 , file extension phải là xlsx ",
+          "precondition": "chỉ có 2 enum là enable và disable",
+        };
+  
+        return {
+          "Total Errors: ": errorData.lengthError,
+          ...errorData,
+        };
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .file-upload {
     height: 100vh;
     width: 100%;
     display: flex;
     align-items: flex-start;
     justify-content: center;
-}
-
-.button-download{
+  }
+  
+  .button-download {
     cursor: pointer;
-}
-.flex-container {
-  display: flex;
-  align-items: center; /* Căn giữa theo chiều dọc */
-}
-
-.flex-container h5{
+  }
+  
+  .flex-container {
+    display: flex;
+    align-items: center; /* Căn giữa theo chiều dọc */
+  }
+  
+  .flex-container h4 {
     color: red;
     margin-right: 10px;
-}
-.file-upload-label {
+  }
+  
+  .file-upload-label {
     padding: 10px;
     border-radius: 3px;
     cursor: pointer;
-}
-
-
-
-.file-upload .file-upload__area {
+  }
+  
+  .file-upload .file-upload__area {
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -121,21 +125,21 @@ export default {
     border: 2px dashed #92b2e2;
     margin-top: 20px;
     margin-bottom: 20px;
-}
-
-.button-upload {
+  }
+  
+  .button-upload {
     border: none;
     border-radius: 2px;
     color: white;
     background-color: #3791be;
-    display: inline-blocl;
+    display: inline-block;
     padding: 5px;
     cursor: pointer;
     margin-top: 10px;
-}
-
-.button-upload:hover {
+  }
+  
+  .button-upload:hover {
     background-color: #096999;
-}
-</style>
-
+  }
+  </style>
+  
